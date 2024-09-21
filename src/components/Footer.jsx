83e4fccab517b5010bcdca4380f1aec0162902f7
George-Oladokun <oladokungeorge8@gmail.com>
2024-09-21 16:31:59 +0100

    last commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = ({ position }) => {
         )} bottom-0 w-full py-[2px] text-center bg-stone-900 text-slate-200`}
       >
         <p className="text-sm md:text-lg">
-          &#169; 2024 Victor Aderibigbe (BytesNova &trade;)
+          &#169; 2024 Oladokun George Abraham (BytesNova &trade;)
         </p>
       </div>
     </>
